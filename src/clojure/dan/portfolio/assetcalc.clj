a1bf1cc602268d4ed04179f716f1ad17ca727431
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2012-02-19 01:00:41 +0400

    Assetcalc

--- src/clojure/dan/portfolio/assetcalc.clj
+++ src/clojure/dan/portfolio/assetcalc.clj
@@ -2,4 +2,25 @@
        :author "Alexander Dovzhikov"}
   dan.portfolio.assetcalc)
 
-; It's still empty, put some data here
+(defn price [bid ask]
+  {:bid bid :ask ask})
+
+(defn bid [prc]
+  (:bid prc))
+
+(defn ask [prc]
+  (:ask prc))
+
+(defn currency [currency-name]
+  {:name currency-name})
+
+;; Program code
+(def rouble (currency "Rouble"))
+
+;; Testing area
+(def expected-bid 29.3)
+(def expected-ask 30.23)
+(def prc (price expected-bid expected-ask))
+
+(println "Expected bid:" expected-bid "; Actual bid:" (bid prc) "; Test:" (= expected-bid (bid prc)))
+(println "Expected ask:" expected-ask "; Actual ask:" (ask prc) "; Test:" (= expected-ask (ask prc)))
