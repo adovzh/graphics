5c6bbb13de223d17101fa690143075273bb94ea4
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2012-02-20 15:33:27 +0400

    Assetcalc

--- src/clojure/dan/portfolio/assetcalc.clj
+++ src/clojure/dan/portfolio/assetcalc.clj
@@ -29,14 +29,51 @@
 (defn create-account [account-name account-currency]
   (Currency. account-name account-currency 0))
 
+;; Portfolio
+
+(defrecord portfolio-entry [entry weight])
+
+(defn flatten-portfolio [portfolio]
+  (cond
+    (list? portfolio) (mapcat flatten-portfolio portfolio)
+    (list? (:entry portfolio)) (flatten-portfolio (:entry portfolio))
+    :default (list portfolio)))
+
 ;; Program code
 (def rouble (create-currency "Rouble"))
 (def euro   (create-currency "Euro"))
 (def dollar (create-currency "Dollar"))
+(def aud    (create-currency "AUD"))
+(def gold   (create-currency "Gold"))
+(def rmf    (create-currency "Funds"))
 
 (def rates {rouble  (create-price 1 1)
             euro    (create-price 39.17  39.56)
-            dollar  (create-price 29.72  30.38)})
+            dollar  (create-price 29.72  30.38)
+            aud     (create-price 31.82  33.2)
+            gold    (create-price 1635   1695)
+            rmf     (create-price 17026.69  17026.69)})
+
+(def main-portfolio
+  (list
+    (portfolio-entry.
+      (list
+        (portfolio-entry.
+          (Account. "Russbank Dollar Classic" dollar 1807.23)
+          1)
+        (portfolio-entry.
+          (Account. "Home Savings AUD" aud 500)
+          1)
+        (portfolio-entry.
+          (Account. "Russbank Savings" rouble (+ 61700 11416.48))
+          1))
+      45)
+    (portfolio-entry.
+      (Account. "Russbank Gold" gold 50)
+      25)
+    (portfolio-entry.
+      (Account. "Mutual Funds" rmf 7.00921)
+      30)))
 
 ;; Testing area
 (def expected-bid 29.3)
@@ -48,3 +85,6 @@
 (println "Dollar bid: " (bid (rates dollar)))
 (println "Euro ask: " (ask (rates euro)))
 (println "Euro ask: " (-> euro rates ask))
+(println main-portfolio)
+(def fportfolio (flatten-portfolio main-portfolio))
+(doseq [p fportfolio] (println p))
